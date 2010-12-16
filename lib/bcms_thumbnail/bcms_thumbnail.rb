5a6cdc62ced819388267be65e0bd591a3f0c6a01
Håkon Lerring <hakon@powow.no>
2010-12-16 14:51:50 +0100

    Add support for fill

--- lib/bcms_thumbnail/bcms_thumbnail.rb
+++ lib/bcms_thumbnail/bcms_thumbnail.rb
@@ -3,42 +3,53 @@ require 'uri'
 module ActionView
   module Helpers
     module_function
-
-    def thumbnail(attachment_obj,geometry = '100x100', quality = 85)
-      thumbnail_engine(attachment_obj, geometry, false, quality)
+    
+    def thumbnail_to_fill(attachment_obj,width,height, quality = 85)
+      thumbnail_engine(attachment_obj, "fill_#{width}x#{height}_#{quality}") do |image|
+        image.quality quality
+        image.resize "#{width}x#{height}^"
+        image.gravity "Center"
+        image.crop "#{width}x#{height}+0+0"
+      end
+    end
+    
+    def thumbnail(attachment_obj,geometry,quality)
+      thumbnail_to_fit(attachment_obj,geometry,quality)
+    end
+    
+    def thumbnail_to_fit(attachment_obj,geometry = '100x100', quality = 85)
+      #thumbnail_engine(attachment_obj, geometry, false, quality)
+      thumbnail_engine(attachment_obj, "fit_#{geometry}_#{quality}") do |image|
+        image.quality quality
+        image.resize geometry
+      end
     end
 
     def thumbnail_square(attachment_obj,geometry = '100', quality = 85)
-      fixed_geometry = geometry
-      if geometry.match(/\D/)
-        fixed_geometry = geometry.split(/\D/)[0]
+      thumbnail_engine(attachment_obj, "square_#{geometry}_#{quality}") do |image|
+        image.quality quality
+          if image[:width] < image[:height]
+            remove = ((image[:height] - image[:width])/2).round
+            image.shave "0x#{remove}"
+          elsif image[:width] > image[:height]
+            remove = ((image[:width] - image[:height])/2).round
+            image.shave "#{remove}x0"
+          end
+          image.resize "#{geometry}x#{geometry}"
       end
-      thumbnail_engine(attachment_obj, geometry, true, quality)
     end
 
-    def thumbnail_engine(attachment_obj,geometry = '100x100',square = false, quality = 85)
+    def thumbnail_engine(attachment_obj, name = nil)
       if ! attachment_obj.blank? && attachment_obj.respond_to?('attachment') && ['jpg','png','gif','bmp'].include?(attachment_obj.attachment.file_extension.downcase)
-        thumbnail_location = "/bcms_thumbnail_cache/#{geometry}/#{attachment_obj.attachment.file_location.gsub(/[\\\/]/,'-')}#{(square) ? '-square' : ''}#{quality}.jpg"
+        thumbnail_location = "/bcms_thumbnail_cache/#{name}/#{attachment_obj.attachment.file_location.gsub(/[\\\/]/,'-')}.jpg"
         if ! File.exists?("#{RAILS_ROOT}/public#{thumbnail_location}")
-          if ! File.exists?("#{RAILS_ROOT}/public/bcms_thumbnail_cache/#{geometry}")
-            FileUtils.mkdir_p("#{RAILS_ROOT}/public/bcms_thumbnail_cache/#{geometry}")
+          if ! File.exists?("#{RAILS_ROOT}/public/bcms_thumbnail_cache/#{name}")
+            FileUtils.mkdir_p("#{RAILS_ROOT}/public/bcms_thumbnail_cache/#{name}")
             FileUtils.chmod 0755, "#{RAILS_ROOT}/public/bcms_thumbnail_cache/"
-            FileUtils.chmod 0755, "#{RAILS_ROOT}/public/bcms_thumbnail_cache/#{geometry}"
+            FileUtils.chmod 0755, "#{RAILS_ROOT}/public/bcms_thumbnail_cache/#{name}"
           end
           image = MiniMagick::Image.from_file("#{RAILS_ROOT}/tmp/uploads/#{attachment_obj.attachment.file_location}")
-          image.quality quality
-          if square
-            if image[:width] < image[:height]
-              remove = ((image[:height] - image[:width])/2).round
-              image.shave "0x#{remove}"
-            elsif image[:width] > image[:height]
-              remove = ((image[:width] - image[:height])/2).round
-              image.shave "#{remove}x0"
-            end
-            image.resize "#{geometry}x#{geometry}"
-          else
-            image.resize geometry
-          end
+          yield image
           image.write("#{RAILS_ROOT}/public#{thumbnail_location}")
           FileUtils.chmod 0644, "#{RAILS_ROOT}/public#{thumbnail_location}"
           URI::escape(thumbnail_location)
